e4aec24036833aa2fee541a4a71c6c9175decef9
Daniel Little <lavinski@live.com.au>
2014-10-26 15:11:18 +1000

    AES crypto interop between js and C#

---
+++ CryptoTest/Controllers/ServiceController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+
+namespace CryptoTest.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        public string Decrypt(InputViewModel model)
+        {
+            var cipherText = Convert.FromBase64String(model.CipherText);
+            var password = Encoding.UTF8.GetBytes(model.Password);
+            var salt = Convert.FromBase64String(model.Salt);
+
+            var crypto = new SimpleCrypto(password, salt);
+
+            var iv = Convert.FromBase64String(model.Iv);
+            if (!Enumerable.SequenceEqual(iv, crypto.IV))
+            {
+                throw new Exception("IVs do not match");
+            }
+
+            var key = Convert.FromBase64String(model.Key);
+            if (!Enumerable.SequenceEqual(key, crypto.Key)) {
+                throw new Exception("Keys do not match");
+            }
+
+
+            var plainText = crypto.Decrypt(cipherText);
+
+            return Encoding.UTF8.GetString(plainText);
+        }
+    }
+
+    public class InputViewModel
+    {
+        public string CipherText { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public string Key { get; set; }
+        public string Iv { get; set; }
+    }
+
+    public class SimpleCrypto
+    {
+        private readonly Encoding encoder = Encoding.Default;
+        private readonly SymmetricAlgorithm algorithm;
+
+        private const int ivBitLength = 128;
+        private const int keyBitLength = 256;
+
+        private const int Iterations = 1000;
+
+        public SimpleCrypto(byte[] password, byte[] salt)
+        {
+            algorithm = Rijndael.Create();
+            algorithm.Padding = PaddingMode.PKCS7;
+            algorithm.Mode = CipherMode.CBC;
+            algorithm.BlockSize = 128;
+            algorithm.KeySize = 256;
+
+            using (var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, Iterations)) {
+                algorithm.IV = rfc2898DeriveBytes.GetBytes(ivBitLength / 8);
+                algorithm.Key = rfc2898DeriveBytes.GetBytes(keyBitLength / 8);
+            }
+        }
+
+        public byte[] IV {
+            get { return algorithm.IV; }
+        }
+
+        public byte[] Key
+        {
+            get { return algorithm.Key; }
+        }
+
+        private static byte[] Transform(byte[] bytes, Func<ICryptoTransform> selectCryptoTransform)
+        {
+            using (var memoryStream = new MemoryStream()) {
+                using (var cryptoStream = new CryptoStream(memoryStream, selectCryptoTransform(), CryptoStreamMode.Write)) {
+                    cryptoStream.Write(bytes, 0, bytes.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        public string Encrypt(string unencrypted)
+        {
+            return Convert.ToBase64String(Encrypt(encoder.GetBytes(unencrypted)));
+        }
+
+        public string Decrypt(string encrypted)
+        {
+            return encoder.GetString(Decrypt(Convert.FromBase64String(encrypted)));
+        }
+
+        public byte[] Encrypt(byte[] buffer)
+        {
+            return Transform(buffer, algorithm.CreateEncryptor);
+        }
+
+        public byte[] Decrypt(byte[] buffer)
+        {
+            return Transform(buffer, algorithm.CreateDecryptor);
+        }
+
+    }
+}
---
+++ CryptoTest/Scripts/Views/Home/Index.js
@@ -0,0 +1,83 @@
+﻿
+// The real code
+
+var message = "SuperSecret!!";
+
+var bytesInSalt = 128 / 8;
+var salt = CryptoJS.lib.WordArray.random(bytesInSalt);
+
+var wordArraySubArray = function (array, index, length) {
+    var hex = array.toString(CryptoJS.enc.Hex);
+    var subHex = hex.substr(index * 2, length * 2);
+    return CryptoJS.enc.Hex.parse(subHex);
+};
+
+var getKeyAndIV = function (password, salt) {
+
+    var ivBitLength = 128;
+    var keyBitLength = 256;
+
+    var ivByteLength = ivBitLength / 8;
+    var keyByteLength = keyBitLength / 8;
+
+    var ivWordLength = ivBitLength / 32;
+    var keyWordLength = keyBitLength / 32;
+
+    var totalWordLength = ivWordLength + keyWordLength;
+
+    var iterations = 1000;
+    var allBits = CryptoJS.PBKDF2(password, salt, { keySize: totalWordLength, iterations: iterations });
+
+    var iv128Bits = wordArraySubArray(allBits, 0, ivByteLength);
+    var key256Bits = wordArraySubArray(allBits, ivByteLength, keyByteLength);
+
+    return {
+        iv: iv128Bits,
+        key: key256Bits
+    };
+};
+
+var skey = getKeyAndIV("Password01", salt);
+
+var data = CryptoJS.AES.encrypt(message, skey.key, { iv: skey.iv, padding: CryptoJS.pad.Pkcs7 }); // , format: JsonFormatter
+
+$(".output_text").val(data.ciphertext.toString(CryptoJS.enc.Base64));
+$(".output_key").val(data.key.toString(CryptoJS.enc.Base64));
+$(".output_iv").val(data.iv.toString(CryptoJS.enc.Base64));
+
+
+var ciphertext = CryptoJS.enc.Base64.parse($(".output_text").val());
+var key = CryptoJS.enc.Base64.parse($(".output_key").val());
+var iv = CryptoJS.enc.Base64.parse($(".output_iv").val());
+
+var params = {
+  ciphertext: ciphertext,
+  salt: ""
+};
+
+var clearText = CryptoJS.AES.decrypt(params, key, { iv: iv, padding: CryptoJS.pad.Pkcs7 });
+
+$(".output2").val(clearText.toString(CryptoJS.enc.Utf8));
+
+var dataToSend = {
+  password: "Password01",
+  salt: salt.toString(CryptoJS.enc.Base64),
+  ciphertext: data.ciphertext.toString(CryptoJS.enc.Base64),
+
+  key: data.key.toString(CryptoJS.enc.Base64),
+  iv: data.iv.toString(CryptoJS.enc.Base64)
+}
+
+var jsonData = JSON.stringify(dataToSend);
+
+$.ajax("/api/service/decrypt", {
+  type: 'post',
+  data: jsonData,
+  contentType: 'application/json',
+  dataType: 'json'
+}).done(function(x) {
+  alert(x);
+}).fail(function(x) {
+  alert(x.responseText);
+});
+
